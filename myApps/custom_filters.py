b002b470e0e21987407eb8be9021056823c922b2
grhey0115 <grhey0115@gmail.com>
2025-01-12 23:24:07 +0800

    delete files

--- myApps/custom_filters.py
+++
@@ -1,8 +0,0 @@
-from django import template
-
-register = template.Library()
-
-@register.filter
-def range_filter(start, end):
-    """Generate a range list for use in templates."""
-    return range(start, end + 1)
